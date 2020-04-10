143fb3db6ae8b291deae1c262cc9a58a0ed772a2
sulthonuladib <adibcyber4rt@gmail.com>
2020-04-11 04:42:57 +0700

    create todo model and db migration

---
+++ db/migrate/20200410214228_create_todos.rb
@@ -0,0 +1,11 @@
+class CreateTodos < ActiveRecord::Migration[6.0]
+  def change
+    create_table :todos do |t|
+      t.string :title
+      t.string :created_by
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
